9d3181c6577cf4bb0a284eca04edeb4d51b9e5db
kelvin handoko <kelvinhandoko03@gmail.com>
2024-04-28 00:24:36 +0700

    docs(music-player-provider): add docs on music player provider

--- lib/providers/music_player_provider.dart
+++ lib/providers/music_player_provider.dart
@@ -7,13 +7,20 @@ import 'package:spotify_clone/services/color_generator.dart';
 import 'package:spotify_clone/services/lyric.dart';
 import 'package:spotify_clone/services/youtube.dart';
 
+/// A provider class that provides the music player state
+/// and the audio player instance
+
 class MusicPlayerProvider extends ChangeNotifier {
+  /// Constructor for the MusicPlayerProvider
   MusicPlayerProvider() {
+    /// Listen to the audio player events
     _audioPlayer.onPositionChanged.listen((Duration duration) async {
       if (duration != _totalDuration) {
         _currentPosition = duration;
         notifyListeners();
       }
+
+      /// kalo antriannya kosong, reset semua state
       if (_queue.isEmpty) {
         _currentPosition = Duration.zero;
         _totalDuration = Duration.zero;
@@ -21,7 +28,9 @@ class MusicPlayerProvider extends ChangeNotifier {
       }
     });
 
+    /// Listen to the audio player events
     _audioPlayer.onPlayerComplete.listen((event) async {
+      /// kalo antriannya lebih dari 1, lanjut ke lagu berikutnya
       if (_queue.length > 1) {
         await next();
         notifyListeners();
@@ -41,9 +50,11 @@ class MusicPlayerProvider extends ChangeNotifier {
   final AudioPlayer _audioPlayer = AudioPlayer();
   AudioPlayer get audioPlayer => _audioPlayer;
 
+  /// The current track that is being played
   Track? _currentTrack;
   Track? get currentTrack => _currentTrack;
 
+  /// The color of the current track
   Color _currentTrackColor = Colors.grey;
   Color get currentTrackColor => _currentTrackColor;
 
@@ -54,6 +65,8 @@ class MusicPlayerProvider extends ChangeNotifier {
   Duration get totalDuration => _totalDuration;
 
   final List<Track> _queue = [];
+
+  /// getter for the queue but exclude the current track
   List<Track> get queue => _queue.isEmpty ? [] : _queue.sublist(1);
 
   Lyric? _lyric;
@@ -62,12 +75,24 @@ class MusicPlayerProvider extends ChangeNotifier {
   String _errorMessage = "";
   String get errorMessage => _errorMessage;
 
+  /// resume the audio player
   void resume() {
     _isPlaying = true;
     _audioPlayer.resume();
     notifyListeners();
   }
 
+  /// Adds a [Track] to the queue either at a specified [index] or at the end if no index is provided.
+  ///
+  /// If an [index] is specified, the track is inserted at that position in the queue.
+  /// If no [index] is given, the queue is cleared, and the track is added as the only item.
+  /// After modifying the queue, the first track in the queue becomes the current track.
+  ///
+  /// Calling this method will notify all listeners of the change.
+  ///
+  /// Parameters:
+  /// - [track]: The `Track` object to add to the queue.
+  /// - [index]: The position at which to insert the track. If null, the queue is cleared and the track is added at the end.
   void addToQueue(Track track, int? index) {
     if (index != null) {
       _queue.insert(index, track);
@@ -79,6 +104,11 @@ class MusicPlayerProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// Removes the track at the specified [index] from the queue.
+  /// After removing the track, the first track in the queue becomes the current track.
+  /// Calling this method will notify all listeners of the change.
+  /// Parameters:
+  /// - [index]: The position of the track to remove from the queue.
   void setToFirst(int currentIndex) {
     _queue.removeRange(0, currentIndex + 1);
     _currentTrack = _queue.first;
@@ -86,10 +116,15 @@ class MusicPlayerProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  /// hanya untuk menghapus error message
   void clearError() {
     _errorMessage = "";
     notifyListeners();
   }
+
+  /// Plays the current track in the queue.
+  /// If the queue is empty, this method does nothing.
+  /// If the current track is null, this method does nothing.
 
   Future<void> play() async {
     try {
@@ -122,18 +157,29 @@ class MusicPlayerProvider extends ChangeNotifier {
     }
   }
 
+  /// Pauses the audio player.
   void pause() {
     _isPlaying = false;
     _audioPlayer.pause();
     notifyListeners();
   }
 
+  /// Seeks to the specified [position] in the audio player.
+  /// After seeking, the audio player resumes playing.
+  /// Calling this method will notify all listeners of the change.
+  /// Parameters:
+  /// - [position]: The position to seek to in the audio player.
   void seek(Duration position) {
     _audioPlayer.seek(position);
     resume();
     notifyListeners();
   }
 
+  /// Plays the previous track in the queue.
+  /// If the queue is empty, this method does nothing.
+  /// If the current track is the first track in the queue, this method does nothing.
+  /// After playing the previous track, the first track in the queue becomes the current track.
+  /// Calling this method will notify all listeners of the change.
   Future<void> next() async {
     if (_queue.isNotEmpty) {
       _queue.removeAt(0);
