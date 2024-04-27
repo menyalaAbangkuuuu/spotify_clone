f3aa7d557a51141eae725e04e454caa4e2fd32ab
kelvin handoko <kelvinhandoko03@gmail.com>
2024-04-28 00:07:47 +0700

    refactor(lyric-page): refactor lyric page
    remove unused property (music player provider) in lyric page.

--- lib/providers/music_player_provider.dart
+++ lib/providers/music_player_provider.dart
@@ -1,6 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:spotify/spotify.dart';
 import 'package:spotify_clone/model/lyric.dart';
 import 'package:spotify_clone/model/music.dart';
@@ -131,6 +130,7 @@ class MusicPlayerProvider extends ChangeNotifier {
 
   void seek(Duration position) {
     _audioPlayer.seek(position);
+    resume();
     notifyListeners();
   }
 
