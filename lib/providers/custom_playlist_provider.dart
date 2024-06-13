3ca7ef25d2e1cc0e9f35d3f38b2f0d2cb46742ec
kelvin handoko <kelvinhandoko03@gmail.com>
2024-06-14 00:12:43 +0700

    feat: get all custom playlist

--- lib/providers/custom_playlist_provider.dart
+++ lib/providers/custom_playlist_provider.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:spotify_clone/model/custom_playlist.dart';
+import 'package:spotify_clone/services/custom_playlist.dart';
 
 //TODO : buat provider untuk custom playlist
 //di provider ini nanti bisa CRUD custom playlist kita
@@ -7,8 +9,11 @@ import 'package:flutter/foundation.dart';
 // untuk semua fetching data itu dari https://yourdocumentationlink.com/path-to-provider-docs
 
 class CustomPlaylistProvider extends ChangeNotifier {
+  final _customPlaylistService = CustomPlaylistService();
+
   CustomPlaylistProvider() {
     _getUser();
+    getAllPlaylist();
   }
 
   User? _user;
@@ -18,8 +23,16 @@ class CustomPlaylistProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  // TODO : buat fungsi untuk mengambil playlist user dari firebase
-  Future<void> getPlaylist() async {}
+  final List<CustomPlaylist> _customPlaylists = [];
+
+  List<CustomPlaylist> get customPlaylists => _customPlaylists;
+
+  Future<void> getAllPlaylist() async {
+    List<CustomPlaylist> playlist =
+        await _customPlaylistService.getAllCustomPlaylists(_user?.uid ?? '');
+    _customPlaylists.addAll(playlist);
+    notifyListeners();
+  }
 
   // TODO: buat fungsi untuk menambahkan playlist user ke firebase
   Future<void> addPlaylist() async {}
--- lib/services/custom_playlist.dart
+++ lib/services/custom_playlist.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:spotify_clone/model/custom_playlist.dart';
 
 abstract class CustomPlaylistServiceBase {
@@ -22,6 +25,60 @@ abstract class CustomPlaylistServiceBase {
 // TODO: implement semua method yang ada pada CustomPlaylistServiceBase
 
 class CustomPlaylistService implements CustomPlaylistServiceBase {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
   @override
-  Future<CustomPlaylist> createCustomPlaylist(String name) {}
+  Future<List<CustomPlaylist>> getAllCustomPlaylists(String userId) async {
+    final querySnapshot = await _db
+        .collection('playlists')
+        .where('userId', isEqualTo: userId)
+        .get();
+    if (querySnapshot.docs.length.isNaN) return [];
+    return querySnapshot.docs
+        .map((doc) => CustomPlaylist.fromJson(doc.data()))
+        .toList();
+  }
+
+  @override
+  Future<void> addSongToCustomPlaylist(String playlistId, String songId) {
+    // TODO: implement addSongToCustomPlaylist
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CustomPlaylist> createCustomPlaylist(String name) {
+    // TODO: implement createCustomPlaylist
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteAllSongsFromCustomPlaylist(String playlistId) {
+    // TODO: implement deleteAllSongsFromCustomPlaylist
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteCustomPlaylist(String id) {
+    // TODO: implement deleteCustomPlaylist
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CustomPlaylistWithTracks> getPlaylistDetail(String id) {
+    // TODO: implement getPlaylistDetail
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeSongFromCustomPlaylist(String playlistId, String songId) {
+    // TODO: implement removeSongFromCustomPlaylist
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CustomPlaylist> updateCustomPlaylist(
+      String? name, String? description, String? imageUrl) {
+    // TODO: implement updateCustomPlaylist
+    throw UnimplementedError();
+  }
 }
